e222ed4114a965acbb7c02505bd3cd3e5fa5f8b3
Bartlomiej <bcich@student.agh.edu.pl>
2025-12-14 15:34:00 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import {useState} from "react";
+
 function App() {
-    let title = 'Wall-E';
-
+    const [title, setTitle] = useState('Wall-E');
+    
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value);
     }
-
+    
+    function getMessage() {
+        if (title.length < 5) {
+            return "Tytuł jest za krótki (min. 5 znaków)";
+        }
+        if (title.length > 15) {
+            return "Tytuł jest za długi (max. 15 znaków)";
+        }
+        return null;
+    }
+    
+    const message = getMessage();
+    
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+            <input type="text" value={title} onChange={handleChange}/>
+            {message && <p style={{color: 'red'}}>{message}</p>}
         </div>
     );
 }
