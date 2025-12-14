0101ff8615f23bacb8b15520593e9ce0b01043e9
Bartlomiej <bcich@student.agh.edu.pl>
2025-12-14 16:00:26 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import {useState} from "react";
 
 function App() {
+    const movies = [
+        {title: "Wall-E"},
+        {title: "Pulp Fiction"},
+        {title: "Matrix"},
+        {title: "1670"},
+    ];
+    
     const [title, setTitle] = useState('Wall-E');
     
     function handleChange(event) {
         setTitle(event.target.value);
     }
     
+    function handleClick() {
+        alert(title);
+    }
+    
     function getMessage() {
         if (title.length < 5) {
-            return "Tytuł jest za krótki (min. 5 znaków)";
+            return {text: "Tytuł jest za krótki (min. 5 znaków)", color: "red"};
         }
         if (title.length > 15) {
-            return "Tytuł jest za długi (max. 15 znaków)";
+            return {text: "Tytuł jest za długi (max. 15 znaków)", color: "red"};
         }
-        return null;
+        return {text: "Tytuł jest ekstra, w sam raz na plakat przed kinem!", color: "green"};
     }
     
     const message = getMessage();
@@ -23,8 +34,13 @@ function App() {
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
+            <p style={{color: message.color}}>{message.text}</p>
             <input type="text" value={title} onChange={handleChange}/>
-            {message && <p style={{color: 'red'}}>{message}</p>}
+            <button onClick={handleClick}>Pokaż tytuł filmu</button>
+            
+            <ul>
+                {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+            </ul>
         </div>
     );
 }
